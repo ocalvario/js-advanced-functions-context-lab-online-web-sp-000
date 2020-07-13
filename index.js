3599ffd2d68d6cb832ccd184d8d775f752b9384f
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-13 00:26:05 +0000

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,57 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(employeeArray) {
+  return {
+    firstName: employeeArray[0],
+    familyName: employeeArray[1],
+    title: employeeArray[2],
+    payPerHour: employeeArray[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+function createEmployeeRecords(recordsArray) {
+  return recordsArray.map(employeeArray => createEmployeeRecord(employeeArray))
+}
+
+function createTimeInEvent(dateStamp) {
+  let date = dateStamp.split(' ');
+  this.timeInEvents.push({
+    type: 'TimeIn',
+    hour: parseInt(date[1]),
+    date: date[0]
+  });
+  return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+  let date = dateStamp.split(' ');
+  this.timeOutEvents.push({
+    type: 'TimeOut',
+    hour: parseInt(date[1]),
+    date: date[0]
+  });
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  let timeIn = this.timeInEvents.find(timeEvent => timeEvent.date === date);
+  let timeOut = this.timeOutEvents.find(timeEvent => timeEvent.date === date);
+  return (timeOut.hour - timeIn.hour)/100;
+}
+
+function wagesEarnedOnDate(date) {
+  const hoursWorked = hoursWorkedOnDate.call(this, date);
+  return hoursWorked * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(empRecord => empRecord.firstName === firstName);
+}
+
+function calculatePayroll (srcArray) {
+  return srcArray.reduce((total, empRecord) => total + allWagesFor.call(empRecord), 0);
+}
